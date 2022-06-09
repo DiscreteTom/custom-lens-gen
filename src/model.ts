c9e254e58b67908a7b55dcca8c1460a200ef975e
DiscreteTom <discrete_tom@outlook.com>
2022-06-09 15:38:08 +0800

    extract type Pillar

--- src/model.ts
+++ src/model.ts
@@ -1,34 +1,36 @@
+export type Pillar = {
+  /** Default: `pillar.name` */
+  id?: string;
+  name: string;
+  questions: {
+    /** Default: `` `${pillar.id}_${i}` `` */
+    id?: string;
+    title: string;
+    description?: string;
+    choices: {
+      /** Default: `` `choice_${i}` `` */
+      id?: string;
+      title: string;
+      /** Provide `displayText` (default: `choice.title`) or `url`. */
+      improvementPlan?: {
+        displayText?: string;
+        url?: string;
+      };
+      helpfulResource?: { displayText?: string; url?: string };
+    }[];
+    riskRules: {
+      /** E.g.: `choice1 && !choice2` */
+      condition: "default" | (string & {});
+      risk: "HIGH_RISK" | "MEDIUM_RISK" | "NO_RISK";
+    }[];
+    helpfulResource?: { displayText?: string; url?: string };
+  }[];
+};
+
 export type Lens = {
   /** Latest version: "2021-11-01" */
   schemaVersion?: "2021-11-01" | (string & {});
   name: string;
   description?: string;
-  pillars: {
-    /** Default: `pillar.name` */
-    id?: string;
-    name: string;
-    questions: {
-      /** Default: `` `${pillar.id}_${i}` `` */
-      id?: string;
-      title: string;
-      description?: string;
-      choices: {
-        /** Default: `` `choice_${i}` `` */
-        id?: string;
-        title: string;
-        /** Provide `displayText` (default: `choice.title`) or `url`. */
-        improvementPlan?: {
-          displayText?: string;
-          url?: string;
-        };
-        helpfulResource?: { displayText?: string; url?: string };
-      }[];
-      riskRules: {
-        /** E.g.: `choice1 && !choice2` */
-        condition: "default" | (string & {});
-        risk: "HIGH_RISK" | "MEDIUM_RISK" | "NO_RISK";
-      }[];
-      helpfulResource?: { displayText?: string; url?: string };
-    }[];
-  }[];
+  pillars: Pillar[];
 };
